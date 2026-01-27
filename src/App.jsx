595b9b193157e8fc829388670e8543a82b4dcedf
JulienF-OC <jucamlau16@gmail.com>
2026-01-27 15:03:57 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
+    </>
+  )
+}
+
+export default App
