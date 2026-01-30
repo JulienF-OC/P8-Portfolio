b1e92fb3d4afd59c2b1e63d7c302f96932fbff63
JulienF-OC <jucamlau16@gmail.com>
2026-01-30 11:25:30 +0100

    Navbar v1  terminée

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-<div className="flex items-center justify-center md:justify-between p-4">
+<div className="flex items-center justify-center md:justify-between px-[15%]">
 
       <div className="text-2xl font-bold">
         Franz <span className="text-accent">Julien</span>
       </div>
 
       <div>
-        <ul className="flex flex-dir-row gap-10">
-          <li className="btn btn-ghost">
+        <ul className=" hidden md:flex flex-dir-row gap-2">
+          <li className="btn btn-ghost font-bold">
             <a href="#">Accueil </a>
             </li>
-          <li className="btn btn-ghost">
-            <a href="#">Accueil </a>
+          <li className="btn btn-ghost  font-bold">
+            <a href="#">À propos </a>
             </li>
-            <li className="btn btn-ghost">
-            <a href="#">Accueil </a>
+            <li className="btn btn-ghost  font-bold">
+            <a href="#">Mes projets </a>
             </li>
-            <li className="btn btn-ghost">
-            <a href="#">Accueil </a>
+            <li className="btn btn-ghost  font-bold">
+            <a href="#">Me contacter </a>
             </li>
         </ul>
       </div>
