2e762cfe4f0be15d736f995b23a4b3f0b6447f97
JulienF-OC <jucamlau16@gmail.com>
2026-01-30 17:03:08 +0100

    Ajout du logo dans la navbar et ajout du favicon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,35 +1,28 @@
 import React from 'react'
+import img from '../assets/f-removebg.png'
 
 function Navbar() {
   return (
     <>
-<div className="flex items-center justify-center md:justify-between px-[15%]">
+      <div className="flex items-center justify-left md:justify-between px-[15%]">
 
-      <div className="text-2xl font-bold">
-        Franz <span className="text-accent">Julien</span>
-      </div>
+        <div className="flex items-center">
+          <img src={img} alt="Logo de Franz Julien" className="w-20 h-20" />
+          <div className="text-2xl font-bold">
+            Franz <span className="text-accent">Julien</span>
+          </div>
+        </div>
 
-      <div>
-        <ul className=" hidden md:flex flex-dir-row gap-2">
-          <li className="btn btn-ghost font-bold">
-            <a href="#">Accueil </a>
-            </li>
-          <li className="btn btn-ghost  font-bold">
-            <a href="#">À propos </a>
-            </li>
-            <li className="btn btn-ghost  font-bold">
-            <a href="#">Mes projets </a>
-            </li>
-            <li className="btn btn-ghost  font-bold">
-            <a href="#">Me contacter </a>
-            </li>
+        <ul className="hidden md:flex flex-row gap-2">
+          <li className="btn btn-ghost font-bold"><a href="#">Accueil</a></li>
+          <li className="btn btn-ghost font-bold"><a href="#">À propos</a></li>
+          <li className="btn btn-ghost font-bold"><a href="#">Mes projets</a></li>
+          <li className="btn btn-ghost font-bold"><a href="#">Me contacter</a></li>
         </ul>
-      </div>
 
       </div>
     </>
   )
 }
-
 
 export default Navbar
